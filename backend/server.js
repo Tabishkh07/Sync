7deb35698412600066bc6b435a1580a3f9db53b8
Mohammed Tabish Khan <tabishkxdev@gmail.com>
2026-08-06 16:19:26 +0530

    setup MongoDB connection and backend config

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const connectDB = require("./config/db");
+require("dotenv").config();
+
 const app = express();
+
+connectDB();
+
+app.get("/", (req, res) => {
+    res.send("SYNC API is running");
+});
 
 const port = 3000;
 
